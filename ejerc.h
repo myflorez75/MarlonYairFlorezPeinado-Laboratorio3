15d0396ec2275551d1e6b76a50cd3463dfdcb4cc
myflorez75 <160801086+myflorez75@users.noreply.github.com>
2024-04-03 19:31:23 -0500

    Primer avance

---
+++ ejerc.h
@@ -0,0 +1,8 @@
+#ifndef EJERC_H
+#define EJERC_H
+
+
+int ejercicio1();
+
+
+#endif // EJERC_H
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <ejerc.h>
+
+using namespace std;
+
+int main()
+{
+    ejercicio1();
+}
